463a9b5a6875bd4908e8137de2811c1e1e17a94a
Leandro Manuel Pagano <138594319+LMPagano@users.noreply.github.com>
2023-08-01 21:31:39 +0200

    Initial Commit

---
+++ RickMortyAppLP/RickMortyAppLPApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickMortyAppLPApp.swift
+//  RickMortyAppLP
+//
+//  Created by Pagano Leandro Manuel on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct RickMortyAppLPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
